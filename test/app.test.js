3ab2ddf1160dffe40d3c3af8a8601bf4676852d7
William <webuntermench@protonmail.com>
2021-08-25 17:57:50 +0300

    getLocalStorageitems() and setLocalStorageItems()

--- test/app.test.js
+++ test/app.test.js
@@ -1,1 +1,27 @@
+/**
+ * @jest-environment jsdom
+ */
+import { Model, View, Controller } from '../lib/app.js'
 
+let myModel
+
+describe('Model > manipulating with localStorage:', () => {
+
+	beforeEach(() => {
+		myModel = new Model()	
+	})
+
+
+	test('getLocalStorageItems should be defined', () => {
+		const items = myModel.getLocalStorageItems()
+		expect(items).toBeDefined()
+		expect(items).not.toBeUndefined()
+	})
+
+	test('setLocalStorageItems should change localStorage', () => {
+		const someObj = {id:1, transaction:"Buy flowers", amount:-15}
+		myModel.setLocalStorageitems(someObj)
+		const items = myModel.getLocalStorageItems()
+		expect(items[0]).toEqual(someObj)
+	})
+})
